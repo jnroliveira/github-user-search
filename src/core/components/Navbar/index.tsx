f71a2d43b79c88019654c8ae71c35bf13cfc3bfc
Júnior de Oliveira <junior_siq90@hotmail.com>
2021-02-11 20:22:09 -0300

    Add navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = () => (
+    <nav className="row main-nav">
+        <div className="main-nav-content">
+            <a href="/" className="main-nav-text">
+                <h2>Bootcamp DevSuperior</h2>
+            </a>
+        </div>
+    </nav>
+);
+
+export default Navbar;
